b0e69f5aa86ae65fc4a85c6d5b524093cfd537bd
venkateshchow <venkateshchow123@gmail.com>
2020-09-20 13:23:35 -0500

    Predicate Changes

--- src/com/java8/predicates/Predicates.java
+++ src/com/java8/predicates/Predicates.java
@@ -1,9 +1,39 @@
 package com.java8.predicates;
 
+import java.util.function.Predicate;
+
 public class Predicates {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int[] x = { 0, 7, 10, 20, 30, 40, 50, 60, 70 };
+		Predicate<Integer> p1 = i -> i > 20;
+		Predicate<Integer> p2 = i -> i % 2 == 0;
+
+		System.out.println("Greater than 20:");
+		method1(p1, x);
+
+		System.out.println("Even Numbers:");
+		method1(p2, x);
+		
+		System.out.println("Not Greater than 20:");
+		method1(p1.negate(), x);
+		
+		System.out.println("Greater than 20 and even:");
+		method1(p1.and(p2), x);
+		
+		System.out.println("Not Greater than 20s and even:");
+		method1(p1.or(p2), x);
+
+	}
+
+	static void method1(Predicate<Integer> p, int[] x) {
+		for (int test : x) {
+			if (p.test(test)) {
+				System.out.println(test);
+			}
+
+		}
 
 	}
 
